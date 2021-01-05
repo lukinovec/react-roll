58993ec6bbec3b8e76d6c9c2464cf5c72df83c68
lukinovec <lukinovec@gmail.com>
2021-01-05 21:37:43 +0100

    first commit

---
+++ src/fetchBorci.js
@@ -0,0 +1,49 @@
+export function fetchBorci() {
+    const borci = [
+        {
+            jmeno: "bitch ass nigga",
+            obrazek: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.0-9/31396665_1720094644718984_6184373523188809728_n.jpg?_nc_cat=111&ccb=2&_nc_sid=09cbfe&_nc_ohc=plCugyWI3U8AX-VrOT9&_nc_ht=scontent-prg1-1.xx&oh=79a42738a106c7acd2825f74278a9f8b&oe=601BD6A6"
+        },
+        {
+            jmeno: "original gangster",
+            obrazek: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.0-9/68616516_1329539863882122_2176236422527188992_o.jpg?_nc_cat=106&ccb=2&_nc_sid=09cbfe&_nc_ohc=_RsPyJAcfhAAX93gfxc&_nc_ht=scontent-prg1-1.xx&oh=0ef5aec2b017bdc29f0b6ef9a9617f99&oe=601AE1F2"
+        },
+        {
+            jmeno: "ultraborec",
+            obrazek: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.0-9/66640668_3109958742354242_1399052284905652224_o.jpg?_nc_cat=103&ccb=2&_nc_sid=09cbfe&_nc_ohc=8WYBMf1OiuUAX-4iF_4&_nc_ht=scontent-prg1-1.xx&oh=d7bc6a055ee0e0507d5967a5a6c05d6d&oe=60191EFB"
+        },
+        {
+            jmeno: "ferex perex",
+            obrazek: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.0-9/118342504_10207821522174219_2509933047182442317_n.jpg?_nc_cat=103&ccb=2&_nc_sid=09cbfe&_nc_ohc=nCXJkaaKoasAX9XaH8E&_nc_ht=scontent-prg1-1.xx&oh=7d0452dc2355b2c78c2759550f9b9859&oe=601900A5"
+        },
+        {
+            jmeno: "kikš",
+            obrazek: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.0-9/82451737_2085027848307648_1622746437073042422_o.jpg?_nc_cat=106&ccb=2&_nc_sid=09cbfe&_nc_ohc=vNjNWAYlazYAX9o1Ixk&_nc_ht=scontent-prg1-1.xx&oh=64079f57dcaa877ce6abd70780d67e94&oe=601B80B4"
+        },
+        {
+            jmeno: "bortlex prefrontální kortex",
+            obrazek: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.0-9/90355167_2570807836493411_5350152859826520064_n.jpg?_nc_cat=104&ccb=2&_nc_sid=09cbfe&_nc_ohc=dcsxXR7ZaZMAX_ue4uy&_nc_ht=scontent-prg1-1.xx&oh=83e2d02994f95e81b0715e0cf6408fb1&oe=601B5EEA"
+        },
+        {
+            jmeno: "dancek srancek",
+            obrazek: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.0-9/83576718_850943368652190_2077766845498130432_o.jpg?_nc_cat=109&ccb=2&_nc_sid=09cbfe&_nc_ohc=uyql9k8iNNUAX-NmR3F&_nc_ht=scontent-prg1-1.xx&oh=3f05ffd98a1c37b9b888c748cb26a5cf&oe=6018A4BD"
+        },
+        {
+            jmeno: "poner boner",
+            obrazek: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Urbanherovantbarreeltje.JPG/1200px-Urbanherovantbarreeltje.JPG"
+        },
+        {
+            jmeno: "transformer ahgahiuahahahaha",
+            obrazek: "https://cdn.discordapp.com/attachments/419870642251497474/796053977812697098/PXL_20201123_145532915.jpg"
+        },
+        {
+            jmeno: "tomex sedlex",
+            obrazek: "https://i.pinimg.com/originals/de/a8/a2/dea8a21cd651a75176e5fd5b61ba8755.jpg"
+        },
+        {
+            jmeno: "kokot vyjebany fuj",
+            obrazek: "https://rythmbot.co/rythm.png"
+        },
+    ]
+    return borci[Math.floor(Math.random() * borci.length)];
+}
